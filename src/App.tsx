cf6ffc38d9e777da53e3aa5232ad4a195bc6dc90
Maël Gangloff <contact@maelgangloff.fr>
2021-08-09 16:02:49 +0200

    lint: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {CovidCard} from './components/CovidCard'
-import {Modal, Button, Jumbotron} from "react-bootstrap"
-import QrReader from "react-qr-reader"
+import React from 'react'
+import { CovidCard } from './components/CovidCard'
+import { Modal, Button, Jumbotron } from 'react-bootstrap'
+import QrReader from 'react-qr-reader'
 
 interface State {
   hcert: string,
@@ -10,8 +10,7 @@ interface State {
 }
 
 class App extends React.Component<object, State> {
-
-  constructor(props: object) {
+  constructor (props: object) {
     super(props)
     this.state = {
       isScanning: false,
@@ -22,8 +21,9 @@ class App extends React.Component<object, State> {
     this.appendHCERT = this.appendHCERT.bind(this)
   }
 
-  appendHCERT(hcert?: string) {
-    const newCards = (hcert ?? this.state.hcert).trim().split('\n').filter(e => e.startsWith('HC1:')).map((e: string) => <CovidCard hcert={e} />)
+  appendHCERT (hcert?: string) {
+    const newCards = (hcert ?? this.state.hcert).trim().split('\n').filter(e => e.startsWith('HC1:')).map((e: string) =>
+      <CovidCard key={e} hcert={e}/>)
     this.setState({
       cards: [...this.state.cards, ...newCards],
       hcert: '',
@@ -31,14 +31,14 @@ class App extends React.Component<object, State> {
     })
   }
 
-  onAppend() {
+  onAppend () {
     if (!this.state.hcert.startsWith('HC1:')) {
       return alert('QR content must starts with HC1:')
     }
     this.appendHCERT(this.state.hcert)
   }
 
-  render() {
+  render () {
     return <>
       <Modal show={this.state.isScanning} animation={false}>
         <Modal.Header closeButton>
@@ -49,27 +49,28 @@ class App extends React.Component<object, State> {
             onScan={hcert => hcert !== null && hcert.startsWith('HC1:') && this.appendHCERT(hcert)}
             onError={console.log}
             facingMode='environment'
-            style={{width: '100%'}}
+            style={{ width: '100%' }}
             showViewFinder={true}
           />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => this.setState({isScanning: false})}>
+          <Button variant="secondary" onClick={() => this.setState({ isScanning: false })}>
             Close
           </Button>
         </Modal.Footer>
       </Modal>
       <Jumbotron fluid={true} className='noprint'>
         <h3>Print EU Digital COVID Certificate</h3>
-        <p>This tool respects your personal data. The COVID certificate is decoded on your device and no information about it is sent anywhere.</p>
+        <p>This tool respects your personal data. The COVID certificate is decoded on your device and no information
+          about it is sent anywhere.</p>
         <div className="input-group input-group">
-          <button className='btn btn-outline-dark' onClick={() => this.setState({isScanning: true})}>📷</button>
+          <button className='btn btn-outline-dark' onClick={() => this.setState({ isScanning: true })}>📷</button>
           <textarea className='form-control' value={this.state.hcert}
-                 onChange={({target}) => this.setState({hcert: target.value})} placeholder="HC1:"/>
+                    onChange={({ target }) => this.setState({ hcert: target.value })} placeholder="HC1:"/>
           <div className="btn-group">
             <Button variant="success" onClick={this.onAppend}>✅</Button>
             <Button variant='secondary' onClick={window.print}>🖨️</Button>
-            <Button variant='danger' onClick={() => this.setState({cards: [], hcert: ''})}>🗑</Button>
+            <Button variant='danger' onClick={() => this.setState({ cards: [], hcert: '' })}>🗑</Button>
           </div>
         </div>
         <br/>
@@ -79,4 +80,4 @@ class App extends React.Component<object, State> {
   }
 }
 
-export default App;
+export default App
--- src/components/CovidCard.tsx
+++ src/components/CovidCard.tsx
@@ -7,188 +7,187 @@ import { EUDCC } from '../types/dgc-combined-schema'
 import QRCode from 'qrcode.react'
 import crypto from 'crypto'
 
-    type DecodedQRCode = {
-    header: {
-        alg: number,
-        kid: string
-    },
-    payload: {
-        iss: string,
-        iat: string,
-        exp: string,
-        hcert: EUDCC
-    },
-    sig: string
+type DecodedQRCode = {
+  header: {
+    alg: number,
+    kid: string
+  },
+  payload: {
+    iss: string,
+    iat: string,
+    exp: string,
+    hcert: EUDCC
+  },
+  sig: string
 }
 
 interface Props {
-    hcert: string
+  hcert: string
 }
+
 interface State {
-    hcert: DecodedQRCode | null
+  hcert: DecodedQRCode | null
 }
 
+function decodeQRCode (hcert: string): DecodedQRCode | null {
+  if (!hcert.startsWith('HC1:')) throw new Error('QR content must starts with HC1.')
+  try {
+    const { value } = cbor.decodeFirstSync(zlib.inflateSync(base45.decode(hcert.substr(4, hcert.length))))
+    const rawPayload = cbor.decodeFirstSync(value[2])
+    const rawHeader = cbor.decodeFirstSync(value[0])
 
-function decodeQRCode(hcert: string): DecodedQRCode | null {
-    if(!hcert.startsWith('HC1:')) throw new Error('QR content must starts with HC1.')
-    try {
-        const {value} = cbor.decodeFirstSync(zlib.inflateSync(base45.decode(hcert.substr(4, hcert.length))))
-        const rawPayload = cbor.decodeFirstSync(value[2])
-        const rawHeader = cbor.decodeFirstSync(value[0])
-
-        const header = {alg: rawHeader.get(1), kid: rawHeader.get(4).toString('base64')}
-        const payload = {
-            iss: rawPayload.get(1),
-            iat: new Date(rawPayload.get(6)*1000).toISOString(),
-            exp: new Date(rawPayload.get(4)*1000).toISOString(),
-            hcert: rawPayload.get(-260).get(1)
-        }
-
-        return {
-            header,
-            payload,
-            sig: value[3].toString('base64')
-        }
-    } catch(e) {
-        return null
+    const header = { alg: rawHeader.get(1), kid: rawHeader.get(4).toString('base64') }
+    const payload = {
+      iss: rawPayload.get(1),
+      iat: new Date(rawPayload.get(6) * 1000).toISOString(),
+      exp: new Date(rawPayload.get(4) * 1000).toISOString(),
+      hcert: rawPayload.get(-260).get(1)
     }
+
+    return {
+      header,
+      payload,
+      sig: value[3].toString('base64')
+    }
+  } catch (e) {
+    return null
+  }
 }
 
 export class CovidCard extends React.Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = { hcert: decodeQRCode(props.hcert) }
+  }
 
-    constructor(props: Props) {
-        super(props)
-        this.state = {hcert: decodeQRCode(props.hcert)}
-    }
-
-    render () {
-        const {hcert} = this.state
-        if(hcert === null) return <></>
-        const {payload: {hcert: certificate}} = hcert
-        const passType = 'v' in certificate ? 'VACCINATION' : 't' in certificate ? 'TEST' : 'RECOVERY'
-        const certData = passType === 'VACCINATION' ? certificate.v : passType === 'TEST' ? certificate.t : certificate.r
-        if(certData === undefined) return <></>
-        const [cert] = certData
-        return <table>
+  render () {
+    const { hcert } = this.state
+    if (hcert === null) return <></>
+    const { payload: { hcert: certificate } } = hcert
+    const passType = 'v' in certificate ? 'VACCINATION' : 't' in certificate ? 'TEST' : 'RECOVERY'
+    const certData = passType === 'VACCINATION' ? certificate.v : passType === 'TEST' ? certificate.t : certificate.r
+    if (certData === undefined) return <></>
+    const [cert] = certData
+    return <table>
+      <tbody>
+      <tr>
+        <td>
+          <h4>EU Digital COVID Certificate</h4>
+          {<QRCode value={this.props.hcert} renderAs="svg" level="L" size={100}/>}
+          <p className="name">{certificate?.nam?.gn} {certificate?.nam?.fn}</p>
+          <p>Date of birth {certificate.dob}</p>
+        </td>
+        <td>
+          <h2>{passType}</h2>
+          <table className="infos">
             <tbody>
+            <tr>
+              <td>Unique certificate identifier</td>
+              <td className="uci">{cert.ci as string}</td>
+            </tr>
+            <tr>
+              <td>Certificate issuer</td>
+              <td>{cert.is as string}</td>
+            </tr>
+            <tr>
+              <td>Country of {passType === 'VACCINATION' ? 'vaccination' : 'test'}</td>
+              <td>{cert.co as string}</td>
+            </tr>
+            {
+              'v' in certificate && <>
                 <tr>
-                    <td>
-                        <h4>EU Digital COVID Certificate</h4>
-                        {<QRCode value={this.props.hcert} renderAs="svg" level="L" size={100} />}
-                        <p className="name">{certificate?.nam?.gn} {certificate?.nam?.fn}</p>
-                        <p>Date of birth {certificate.dob}</p>
-                    </td>
-                    <td>
-                        <h2>{passType}</h2>
-                        <table className="infos">
-                            <tbody>
-                            <tr>
-                                <td>Unique certificate identifier</td>
-                                <td className="uci">{cert.ci as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Certificate issuer</td>
-                                <td>{cert.is as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Country of {passType === 'VACCINATION' ? 'vaccination' : 'test'}</td>
-                                <td>{cert.co as string}</td>
-                            </tr>
-                            {
-                            'v' in certificate && <>
-                            <tr>
-                                <td>Disease or agent targeted</td>
-                                <td>{cert.tg as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Vaccine or prophylaxis</td>
-                                <td>{cert.vp as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Vaccine product</td>
-                                <td>{cert.mp as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Vaccine marketing authorization holder</td>
-                                <td>{cert.ma as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Number in a series of doses</td>
-                                <td>{cert.dn as string}</td>
-                            </tr>
-                            <tr>
-                                <td>The overall number of doses</td>
-                                <td>{cert.sd as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Date of vaccination</td>
-                                <td>{cert.dt as string}</td>
-                            </tr>
-                            </>
-                        }
-                        {
-                            't' in certificate && <>
-                            <tr>
-                                <td>Disease or agent targeted</td>
-                                <td>{cert.tg as string}</td>
-                            </tr>
-                            <tr>
-                                <td>The type of test</td>
-                                <td>{cert.tt as string}</td>
-                            </tr>
-                            <tr>
-                                <td>The name of the NAAT used</td>
-                                <td>{cert.nm as string}</td>
-                            </tr>
-                            <tr>
-                                <td>The device identifier of the RAT used</td>
-                                <td>{cert.ma as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Date and time of the test sample collection</td>
-                                <td>{cert.sc as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Result of the test</td>
-                                <td>{cert.tr as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Testing centre or facility</td>
-                                <td>{cert.tc as string}</td>
-                            </tr>
-                            </>
-                        }
-                        {
-                            'r' in certificate && <>
-                            <tr>
-                                <td>Disease or agent targeted</td>
-                                <td>{cert.tg as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Date of the holder's first positive</td>
-                                <td>{cert.fr as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Certificate issuer</td>
-                                <td>{cert.is as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Certificate valid from</td>
-                                <td>{cert.df as string}</td>
-                            </tr>
-                            <tr>
-                                <td>Certificate valid until</td>
-                                <td>{cert.du as string}</td>
-                            </tr>
-                            </>
-                        }
-                            </tbody>
-                        </table>
-                        <p className="fingerprint">{crypto.createHash('sha256').update(cert.co.toUpperCase() + cert.ci).digest('hex')}</p>
-                    </td>
+                  <td>Disease or agent targeted</td>
+                  <td>{cert.tg as string}</td>
                 </tr>
+                <tr>
+                  <td>Vaccine or prophylaxis</td>
+                  <td>{cert.vp as string}</td>
+                </tr>
+                <tr>
+                  <td>Vaccine product</td>
+                  <td>{cert.mp as string}</td>
+                </tr>
+                <tr>
+                  <td>Vaccine marketing authorization holder</td>
+                  <td>{cert.ma as string}</td>
+                </tr>
+                <tr>
+                  <td>Number in a series of doses</td>
+                  <td>{cert.dn as string}</td>
+                </tr>
+                <tr>
+                  <td>The overall number of doses</td>
+                  <td>{cert.sd as string}</td>
+                </tr>
+                <tr>
+                  <td>Date of vaccination</td>
+                  <td>{cert.dt as string}</td>
+                </tr>
+              </>
+            }
+            {
+              't' in certificate && <>
+                <tr>
+                  <td>Disease or agent targeted</td>
+                  <td>{cert.tg as string}</td>
+                </tr>
+                <tr>
+                  <td>The type of test</td>
+                  <td>{cert.tt as string}</td>
+                </tr>
+                <tr>
+                  <td>The name of the NAAT used</td>
+                  <td>{cert.nm as string}</td>
+                </tr>
+                <tr>
+                  <td>The device identifier of the RAT used</td>
+                  <td>{cert.ma as string}</td>
+                </tr>
+                <tr>
+                  <td>Date and time of the test sample collection</td>
+                  <td>{cert.sc as string}</td>
+                </tr>
+                <tr>
+                  <td>Result of the test</td>
+                  <td>{cert.tr as string}</td>
+                </tr>
+                <tr>
+                  <td>Testing centre or facility</td>
+                  <td>{cert.tc as string}</td>
+                </tr>
+              </>
+            }
+            {
+              'r' in certificate && <>
+                <tr>
+                  <td>Disease or agent targeted</td>
+                  <td>{cert.tg as string}</td>
+                </tr>
+                <tr>
+                  <td>Date of the holder&rsquo;s first positive</td>
+                  <td>{cert.fr as string}</td>
+                </tr>
+                <tr>
+                  <td>Certificate issuer</td>
+                  <td>{cert.is as string}</td>
+                </tr>
+                <tr>
+                  <td>Certificate valid from</td>
+                  <td>{cert.df as string}</td>
+                </tr>
+                <tr>
+                  <td>Certificate valid until</td>
+                  <td>{cert.du as string}</td>
+                </tr>
+              </>
+            }
             </tbody>
-        </table>
-    }
-
+          </table>
+          <p
+            className="fingerprint">{crypto.createHash('sha256').update(cert.co.toUpperCase() + cert.ci).digest('hex')}</p>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  }
 }
